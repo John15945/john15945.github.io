718840adf9da7a133074cbfa0e13d29bf65d63d5
青春风1999 <2696410507@qq.com>
2021-08-23 15:22:39 +0800

    update website page

--- README.md
+++
@@ -1,5 +0,0 @@
-# website
-a website by myselef
-
-
-[visit](https://john15945.github.io/website/)
---
+++ config/index.js
@@ -0,0 +1,10 @@
+window.app_config={
+  "NODE_ENV": "production",
+  "VUE_APP_SHOW_CONSOLE": false,
+  "VUE_APP_BASE_URL": "http://192.168.1.118:9900/",
+  "VUE_APP_MOCK_URL": "/mock",
+  "VUE_APP_PUBLIC_PATH": "./",
+  "VUE_APP_ROUTER_BASE": "/",
+  "VUE_APP_USE_SOURCEMAP": true,
+  "VUE_APP_USE_PXTOREM": false
+}
